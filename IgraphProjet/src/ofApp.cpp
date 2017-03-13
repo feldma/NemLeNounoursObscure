65ddb291ee7384fc7fe2b7796f4d4398b7f91ae0
ALFEL5 <alexis.feldmar.1@ulaval.ca>
2017-03-13 03:55:32 +0100

    oui

--- IgraphProjet/src/ofApp.cpp
+++ IgraphProjet/src/ofApp.cpp
@@ -2,34 +2,65 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetVerticalSync(true);
 	ofBackground(20);
-	m_camera.setGlobalPosition(0, 0, 0);
+	m_camera.setGlobalPosition(0, 0, 5);
 	m_lookAt = m_camera.getLookAtDir();
 	m_backgroundAllowed = false;
 
 	ofBoxPrimitive box;
 
-	box.set(20,20,20);
-	box.setPosition(0,0,0);	
+	box.set(5,5,5);
+	box.setPosition(0,0,0);
 	m_scene.push_back(box);
 
 	m_gui = new ofxDatGui(ofxDatGuiAnchor::TOP_LEFT);
 	m_gui->addButton("Ouvrir image");
 	m_gui->addFolder("Images disponibles:", ofColor::white);
 	m_gui->addButton("Prendre un screenshot");
-	std::vector<std::string> opts = { "3D", "Dessin", "Primitives" };
+	m_gui->addButton("Importer un modele");
+	std::vector<std::string> opts = { "3D", "Selection", "Primitives" };
 	m_gui->addDropdown("Options", opts);
 	m_gui->onButtonEvent(this, &ofApp::buttonEvent);
+	m_gui->addSlider("FOV:", 0, 120);
+	m_gui->addSlider("FarClip", 0, 1);
+	m_gui->addSlider("NearClip", 0, 1);
+	m_gui->addSlider("RotationX", 0, 360);
+	m_gui->addSlider("RotationY", 0, 360);
+	m_gui->addSlider("RotationZ", 0, 360);
+	m_gui->addSlider("TransformationX", -200, 200);
+	m_gui->addSlider("TranformationY", -200, 200);
+	m_gui->addSlider("TransformationZ", -200, 200);
+	m_gui->addSlider("Scale", 0, 10);
+	m_gui->onSliderEvent(this, &ofApp::sliderEvent);
+	std::vector<std::string> optsCamera = { "4:3", "16:9" };
+	m_gui->addDropdown("Ratio d'aspect", optsCamera);
+
 	m_gui->onDropdownEvent(this, &ofApp::dropdownEvent);
+
+	m_createPrimitive = new ofxDatGui(ofxDatGuiAnchor::TOP_RIGHT);
+	m_createPrimitive->addTextInput("X:", "");
+	m_createPrimitive->addTextInput("Y:", "");
+	m_createPrimitive->addTextInput("Z:", "");
+	m_createPrimitive->addTextInput("Taille:", "");
+	m_createPrimitive->addTextInput("Hauteur (cone):", "");
+	std::vector<std::string> optsCreate = { "Cube", "Sphere", "Cone" };
+	m_createPrimitive->addDropdown("Ajouter Element", optsCreate);
+	m_createPrimitive->onDropdownEvent(this, &ofApp::dropdownEvent);
+
+	m_rotateX = m_rotateY = m_rotateZ = 90;
+	m_transformX = m_transformY = m_transformZ = 0;
+
 	m_hideMenu = true;
 	m_3d = true;
-	m_dessin = false;
+	m_selection = false;
 	m_primitive = false;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	m_camera.lookAt(m_lookAt);
+
 }
 
 //--------------------------------------------------------------
@@ -38,7 +69,11 @@ void ofApp::draw(){
 	if (m_backgroundAllowed)
 		m_background.draw(0,0);
 	for (std::vector<of3dPrimitive>::iterator it = m_scene.begin(); it != m_scene.end(); it++)
+	{
+		applyChanges(*it);
 		it->draw();
+	}
+	m_model.draw(OF_MESH_FILL);
 	m_camera.end();
 }
 
@@ -80,29 +115,17 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-	if (button == 2)
+	if (button == 2 && m_3d)
 	{
-		if (x > m_lastMouse.x)
-		{
-			m_lookAt.x += 10;
-		}
+		if (x > m_lastMouse.x && m_3d)
+			m_lookAt.x -= 0.01;
 		else
-		{
-			std::cout << "moins" << std::endl;
-			m_lookAt.x -= 10;
-		}
-	}
-	if (button == 0)
-	{
-		if (y > m_lastMouse.y)
-		{
-			m_lookAt.y += 10;
-		}
-		else
-		{
-			m_lookAt.y -= 10;
-		}
+			m_lookAt.x += 0.01;
 		m_lastMouse.x = x;
+		if (y > m_lastMouse.y)
+			m_lookAt.y -= 0.01;
+		else
+			m_lookAt.y += 0.01;
 		m_lastMouse.y = y;
 	}
 }
@@ -117,9 +140,15 @@ void ofApp::mousePressed(int x, int y, int button){
 void ofApp::mouseReleased(int x, int y, int button){
 	if (button == 0 && m_primitive)
 	{
-		ofSetHexColor(0xff0000);
-		ofFill();
-		ofRect(m_lastMouseForDrawing.x, m_lastMouseForDrawing.y, x - m_lastMouseForDrawing.x, y - m_lastMouseForDrawing.y);
+		//ofPushMatrix();
+		//ofSetHexColor(0xff0000);
+		//ofFill();
+		//ofDrawPlane(m_lastMouseForDrawing.x, m_lastMouseForDrawing.y, x - m_lastMouseForDrawing.x, y - m_lastMouseForDrawing.y);
+		//ofPopMatrix();
+		//ofPlanePrimitive plane;
+		//plane.set(x - m_lastMouseForDrawing.x, y - m_lastMouseForDrawing.y);
+		//plane.setPosition(x,y, 0);
+		//m_scene.push_back(plane);
 	}
 }
 
@@ -157,10 +186,10 @@ void ofApp::loadImage()
 	ZeroMemory(&ofn, sizeof(ofn));
 	ofn.lStructSize = sizeof(ofn);
 	ofn.hwndOwner = NULL;  // If you have a window to center over, put its HANDLE here
-	ofn.lpstrFilter = "Text Files\0*.txt\0Any File\0*.*\0";
+	ofn.lpstrFilter = "Any File\0*.*\0";
 	ofn.lpstrFile = filename;
 	ofn.nMaxFile = MAX_PATH;
-	ofn.lpstrTitle = "Select a File, yo!";
+	ofn.lpstrTitle = "Select a File";
 	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
 
 	if (GetOpenFileNameA(&ofn))
@@ -171,6 +200,29 @@ void ofApp::loadImage()
 		m_imagesOpen.push_back(img);
 		m_images.push_back(filename);
 		m_gui->getFolder("Images disponibles:")->addButton(filename);
+	}
+}
+
+void ofApp::loadModel()
+{
+	char filename[MAX_PATH];
+
+	OPENFILENAME ofn;
+	ZeroMemory(&filename, sizeof(filename));
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = NULL;  // If you have a window to center over, put its HANDLE here
+	ofn.lpstrFilter = "Obj file\0*.*\0";
+	ofn.lpstrFile = filename;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrTitle = "Select a File";
+	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
+
+	if (GetOpenFileNameA(&ofn))
+	{
+		m_model.loadModel(filename, true);
+		m_model.calculateDimensions();
+		m_model.setPosition(0,0,0);
 	}
 }
 
@@ -185,10 +237,11 @@ void ofApp::buttonEvent(ofxDatGuiButtonEvent e)
 		tmp.grabScreen(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
 		tmp.save("export.png");
 	}
+	else if (e.target->getLabel() == "Importer un modele")
+		loadModel();
 	else
 	{
-		std::cout << "text" << std::endl;
-		m_background.load(m_images[0]);
+		m_background.load(e.target->getLabel());
 		m_backgroundAllowed = true;
 		ofTexture tex;
 		tex.loadData(m_imagesOpen[0].getPixels());
@@ -201,19 +254,151 @@ void ofApp::dropdownEvent(ofxDatGuiDropdownEvent e)
 	if (e.target->getLabel() == "3D")
 	{
 		m_3d = true;
-		m_dessin = false;
+		m_selection = false;
 		m_primitive = false;
+		m_createPrimitive->setVisible(true);
 	}
 	if (e.target->getLabel() == "Dessin")
 	{
 		m_3d = false;
-		m_dessin = true;
+		m_selection = true;
 		m_primitive = false;
+		m_createPrimitive->setVisible(false);
 	}
-	if (e.target->getLabel() == "Primitive")
+	if (e.target->getLabel() == "Primitives")
 	{
 		m_3d = false;
-		m_dessin = false;
+		m_selection = false;
 		m_primitive = true;
+		m_createPrimitive->setVisible(false);
 	}
+
+	if (e.target->getLabel() == "Cube")
+	{
+		ofBoxPrimitive box;
+		std::string x = m_createPrimitive->getTextInput("X:")->getText();
+		std::string y = m_createPrimitive->getTextInput("Y:")->getText();
+		std::string z = m_createPrimitive->getTextInput("Z:")->getText();
+		std::string size = m_createPrimitive->getTextInput("Taille:")->getText();
+		if (x != "" && y != "" && z != "")
+		{
+			box.setPosition(ofToDouble(x), ofToDouble(y), ofToDouble(z));
+			if (size != "")
+				box.setScale(ofToDouble(size));
+			m_scene.push_back(box);
+		}
+	}
+	if (e.target->getLabel() == "Sphere")
+	{
+		ofSpherePrimitive sphere;
+		std::string x = m_createPrimitive->getTextInput("X:")->getText();
+		std::string y = m_createPrimitive->getTextInput("Y:")->getText();
+		std::string z = m_createPrimitive->getTextInput("Z:")->getText();
+		std::string size = m_createPrimitive->getTextInput("Taille:")->getText();
+		if (x != "" && y != "" && z != "")
+		{
+			sphere.setPosition(ofToDouble(x), ofToDouble(y), ofToDouble(z));
+			if (size != "")
+				sphere.setRadius(ofToDouble(size));
+			m_scene.push_back(sphere);
+		}
+	}
+	if (e.target->getLabel() == "Cone")
+	{
+		ofConePrimitive cone;
+		std::string x = m_createPrimitive->getTextInput("X:")->getText();
+		std::string y = m_createPrimitive->getTextInput("Y:")->getText();
+		std::string z = m_createPrimitive->getTextInput("Z:")->getText();
+		std::string size = m_createPrimitive->getTextInput("Taille:")->getText();
+		std::string height = m_createPrimitive->getTextInput("Hauteur")->getText();
+		if (x != "" && y != "" && z != "")
+		{
+			cone.setPosition(ofToDouble(x), ofToDouble(y), ofToDouble(z));
+			if (height != "")
+				cone.setHeight(ofToDouble(height));
+			if (size != "")
+				cone.setRadius(ofToDouble(size));
+			m_scene.push_back(cone);
+		}
+	}
+	if (e.target->getLabel() == "4:3")
+	{
+		m_camera.setAspectRatio(4.0f/3.0f);
+	}
+	if (e.target->getLabel() == "16:9")
+	{
+		m_camera.setAspectRatio(16.0f/9.0f);
+	}
+}
+
+void ofApp::sliderEvent(ofxDatGuiSliderEvent e)
+{
+	if (e.target->getLabel() == "FOV:")
+	{
+		float fov = m_gui->getSlider("FOV:")->getValue();
+		m_camera.setFov(fov);
+	}
+	if (e.target->getLabel() == "FarClip")
+	{
+		float farClip = m_gui->getSlider("FarClip")->getValue();
+		m_camera.setFarClip(farClip);
+	}
+	if (e.target->getLabel() == "NearClip")
+	{
+		float nearClip = m_gui->getSlider("NearClip")->getValue();
+		m_camera.setNearClip(nearClip);
+	}
+	if (e.target->getLabel() == "RotationX")
+	{
+		m_rotateX = m_gui->getSlider("RotationX")->getValue();
+	}
+	if (e.target->getLabel() == "RotationY")
+	{
+		m_rotateY = m_gui->getSlider("RotationY")->getValue();
+
+	}
+	if (e.target->getLabel() == "RotationZ")
+	{
+		m_rotateZ = m_gui->getSlider("RotationZ")->getValue();
+	}
+	if (e.target->getLabel() == "TransformationX")
+	{
+		m_transformX = m_gui->getSlider("TransformationX")->getValue();
+	}
+	if (e.target->getLabel() == "TransformationY")
+	{
+		m_transformY = m_gui->getSlider("TransformationY")->getValue();
+	}
+	if (e.target->getLabel() == "TransformationZ")
+	{
+		m_transformZ = m_gui->getSlider("TransformationZ")->getValue();
+	}
+	if (e.target->getLabel() == "Scale")
+	{
+		m_scale = m_gui->getSlider("Scale")->getValue();
+	}
+}
+
+void ofApp::applyTransformation(of3dPrimitive prim)
+{
+	prim.setPosition(prim.getGlobalPosition().x + m_transformX, prim.getGlobalPosition().y + m_transformY, prim.getGlobalPosition().z + m_transformZ);
+}
+
+void ofApp::applyRotation(of3dPrimitive prim)
+{
+	prim.rotate(m_rotateX, ofVec3f(1,0,0));
+	prim.rotate(m_rotateY, ofVec3f(0,1,0));
+	prim.rotate(m_rotateZ, ofVec3f(0,0,1));
+}
+
+void ofApp::applyScale(of3dPrimitive prim)
+{
+	prim.setScale(m_scale);
+}
+
+void ofApp::applyChanges(of3dPrimitive prim)
+{
+	applyTransformation(prim);
+	applyRotation(prim);
+	applyScale(prim);
 }
--- IgraphProjet/src/ofApp.h
+++ IgraphProjet/src/ofApp.h
@@ -4,6 +4,7 @@
 #include "ofMain.h"
 #include "ofxGui.h"
 #include "ofxDatGui.h"
+#include "ofxAssimpModelLoader.h"
 
 class ofApp : public ofBaseApp{
 
@@ -31,6 +32,19 @@ private:
 	ofImage m_background;
 	bool m_backgroundAllowed;
 
+	float m_rotateX;
+	float m_rotateY;
+	float m_rotateZ;
+	float m_transformX;
+	float m_transformY;
+	float m_transformZ;
+	float m_scale;
+
+	void applyChanges(of3dPrimitive);
+	void applyTransformation(of3dPrimitive);
+	void applyRotation(of3dPrimitive);
+	void applyScale(of3dPrimitive);
+
 	//camera
 	ofCamera m_camera;
 	bool m_right;
@@ -40,19 +54,23 @@ private:
 
 	//opening files
 	std::vector<std::string> m_images;
+	ofxAssimpModelLoader m_model;
 	void loadImage();
+	void loadModel();
 
 	//gui
 	ofxDatGui *m_gui;
+	ofxDatGui *m_createPrimitive;
 	ofxDatGuiScrollView *m_scrollView;
 	bool m_hideMenu;
 
 	void buttonEvent(ofxDatGuiButtonEvent e);
 	void dropdownEvent(ofxDatGuiDropdownEvent e);
+	void sliderEvent(ofxDatGuiSliderEvent e);
 
 	//mode
 	bool m_3d;
-	bool m_dessin;
+	bool m_selection;
 	bool m_primitive;
 
 	ofVec2f m_lastMouseForDrawing;
